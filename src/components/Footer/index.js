4c6d55f9276609420b4569defacd0a642776e35d
André lima <andrelimashow@gmail.com>
2022-11-30 19:50:03 -0300

    four commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+
+//ICONES
+import add from '../../assets/add.png';
+import save from '../../assets/save.png';
+
+export default function Footer({ icon, onPress }){
+    return(
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.button} onPress={onPress}>
+                <Image source={ icon == 'add' ? add : save } style={styles.image}/>
+            </TouchableOpacity>
+
+            <Text style={styles.text}>
+                Organizando sua vida
+            </Text>
+        </View>
+    )
+}
